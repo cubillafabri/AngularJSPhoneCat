629381d3f4fb05e40bcf73dac1427b03402f1b5e
devcubilla <fabriziocubilla@gmail.com>
2023-05-01 14:52:04 -0400

    add img eventhandler

--- js/phone-detail/controllers/phone-detail.controller.js
+++ js/phone-detail/controllers/phone-detail.controller.js
@@ -5,7 +5,17 @@ angular.module('phoneDetail')
       se.phoneId = rp.phoneId;
       hp.get('phones/' + rp.phoneId + '.json').then(function(response) {
         se.phone = response.data;
+        se.setImage(se.phone.images[0]);
         console.log(se.phone);
       });
+      // method (to be used as event handler) that will change the value of mainImageUrl
+      se.setImage = function(imageUrl){
+        se.mainImageUrl = imageUrl;
+      }
+
+      se.onDblclick = function(imageUrl){
+        alert(imageUrl);
+      }
+      
     }])
 ;
