09ca2e9d317844f59d37ad0b9369009cf1dc535f
devcubilla <fabriziocubilla@gmail.com>
2023-05-01 14:30:32 -0400

    add phonedetail template

--- js/phone-detail/controllers/phone-detail.controller.js
+++ js/phone-detail/controllers/phone-detail.controller.js
@@ -1,7 +1,11 @@
 'use strict'
-// Define the `phoneList` module for controllers
+// Define the `phoneDetail` module for controllers
 angular.module('phoneDetail')
-    .controller('PhoneDetailController',['$scope', '$routeParams' ,function(se, rp){
+    .controller('PhoneDetailController',['$scope', '$routeParams', '$http' ,function(se, rp, hp){
       se.phoneId = rp.phoneId;
+      hp.get('phones/' + rp.phoneId + '.json').then(function(response) {
+        se.phone = response.data;
+        console.log(se.phone);
+      });
     }])
 ;
--- js/phone-detail/phone-detail.module.js
+++ js/phone-detail/phone-detail.module.js
@@ -1,1 +1,2 @@
-angular.module('phoneDetail', ['ngRoute']);
+angular.module('phoneDetail', ['ngRoute']); // buena practica si queremos copiar este modulo a otro
+//angular.module('phoneDetail', []); //igual funciona porque nuestro phone module lo carga.
